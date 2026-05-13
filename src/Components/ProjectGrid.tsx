42afa31c0ecf749c59240c3cfaa1a7a7ae76fe12
Vraj <vraj95soni@gmail.com>
2026-05-13 22:01:26 +0530

    Grid 1x1 for small screen

--- src/Components/ProjectGrid.tsx
+++ src/Components/ProjectGrid.tsx
@@ -5,7 +5,7 @@ export const ProjectGrid = () => {
   return (
     <>
       <div className="pt-10"></div>
-      <section className="grid grid-cols-2 gap-10 px-10">
+      <section className="grid grid-cols-1 sm:grid-cols-2 gap-10 px-10 pb-10">
         {projectsData.map((project) => (
           <ProjectCard
             id={project.id}

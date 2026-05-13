9a26ad7a5ba71991bde965510e1f26ca1f924fc2
Vraj <vraj95soni@gmail.com>
2026-05-13 15:58:38 +0530

    Fixing header for mobile

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -25,7 +25,7 @@ export const Header = () => {
           <li>
             <button
               role="link"
-              className="text-2xl relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100"
+              className="text-sm sm:text-lg md:text-2xl relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100"
               onClick={() => navigate("/projects")}
             >
               Projects
@@ -34,7 +34,7 @@ export const Header = () => {
           <li>
             <button
               role="link"
-              className="text-2xl relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100"
+              className="text-sm sm:text-lg md:text-2xl relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100"
               onClick={() => navigate("/contact-us")}
             >
               Contact Us
